94a97067e579d7eb91293af8069237b72ee7db90
Sebastien Orban <ecrire@sebastienorban.com>
2016-03-03 07:12:22 +0100

    More is moved to use-package, again, cleaner. Now to do the annoying thing : finding last packages still only via package.el directly

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
 )
 
 ;; init packages
-(setq packages-list '(dash undo-tree flycheck haskell-mode helm markdown-mode org paradox projectile use-package ace-jump-mode ack cider clj-refactor clojure-snippets company evil-easymotion evil-smartparens smartparens fish-mode helm-ack helm-company helm-flycheck helm-projectile jedi rainbow-delimiters rust-mode yaml-mode zenburn-theme))
+(setq packages-list '(dash undo-tree org paradox use-package clojure-snippets zenburn-theme))
 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -45,54 +45,55 @@
 ;;                            ;;
 ;; GROUP: Convenience -> Helm ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'helm)
 
-;; must set before helm-config,  otherwise helm use default
-;; prefix "C-x c", which is inconvenient because you can
-;; accidentially pressed "C-x C-c"
-(setq helm-command-prefix-key "C-c h")
+(use-package helm
+  :ensure t
+  :init
+  ;; must set before helm-config,  otherwise helm use default
+  ;; prefix "C-x c", which is inconvenient because you can
+  ;; accidentially pressed "C-x C-c"
+  (setq helm-command-prefix-key "C-c h")
+  :config
+  (require 'helm-config)
+  (require 'helm-eshell)
+  (require 'helm-files)
+  (require 'helm-grep)
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
-(require 'helm-config)
-(require 'helm-eshell)
-(require 'helm-files)
-(require 'helm-grep)
+  (define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
+  (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
+  (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
+  (setq
+   helm-google-suggest-use-curl-p t
+   helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
+   helm-quick-update t ; do not display invisible candidates
+   helm-idle-delay 0.01 ; be idle for this many seconds, before updating in delayed sources.
+   helm-input-idle-delay 0.01 ; be idle for this many seconds, before updating candidate buffer
+   helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+   helm-split-window-default-side 'other ;; open helm buffer in another window
+   helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+   helm-buffers-favorite-modes (append helm-buffers-favorite-modes
+				       '(picture-mode artist-mode))
+   helm-candidate-number-limit 200 ; limit the number of displayed canidates
+   helm-M-x-requires-pattern 0     ; show all candidates when set to 0
+   helm-boring-file-regexp-list
+   '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
+   helm-ff-file-name-history-use-recentf t
+   helm-move-to-line-cycle-in-source t ; move to end or beginning of source
+					; when reaching top or bottom of source.
+   ido-use-virtual-buffers t      ; Needed in helm-buffers-list
+   helm-buffers-fuzzy-matching t          ; fuzzy matching buffer names when non--nil
+					; useful in helm-mini that lists buffers
+   )
+  (global-set-key (kbd "M-x") 'helm-M-x)
 
-(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
-(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
-(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
-(setq
- helm-google-suggest-use-curl-p t
- helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
- helm-quick-update t ; do not display invisible candidates
- helm-idle-delay 0.01 ; be idle for this many seconds, before updating in delayed sources.
- helm-input-idle-delay 0.01 ; be idle for this many seconds, before updating candidate buffer
- helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+  ;; Save current position to mark ring when jumping to a different place
+  (add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
 
- helm-split-window-default-side 'other ;; open helm buffer in another window
- helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
- helm-buffers-favorite-modes (append helm-buffers-favorite-modes
-                                     '(picture-mode artist-mode))
- helm-candidate-number-limit 200 ; limit the number of displayed canidates
- helm-M-x-requires-pattern 0     ; show all candidates when set to 0
- helm-boring-file-regexp-list
- '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
- helm-ff-file-name-history-use-recentf t
- helm-move-to-line-cycle-in-source t ; move to end or beginning of source
-                                        ; when reaching top or bottom of source.
- ido-use-virtual-buffers t      ; Needed in helm-buffers-list
- helm-buffers-fuzzy-matching t          ; fuzzy matching buffer names when non--nil
-                                        ; useful in helm-mini that lists buffers
- )
-(global-set-key (kbd "M-x") 'helm-M-x)
-
-;; Save current position to mark ring when jumping to a different place
-(add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
-
-(helm-mode 1)
+  (helm-mode 1))
 
 ;; Evil mode
 (use-package evil
@@ -104,7 +105,9 @@
   (add-hook 'paradox-menu-mode-hook 'evil-emacs-state)
   (add-hook 'cider-repl-mode-hook 'evil-emacs-state)
   :config
-   (evil-mode 1))
+  (use-package evil-easymotion
+    :ensure t)
+  (evil-mode 1))
 
 ;; YASNippet
 (use-package yasnippet
@@ -116,23 +119,31 @@
 (use-package company
   :ensure t
   :init
-  (add-hook 'after-init-hook 'global-company-mode))
+  (add-hook 'after-init-hook 'global-company-mode)
+  (use-package helm-company
+    :ensure t))
 
 ;; better linter
 (use-package flycheck
   :ensure t
   :init
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  (use-package helm-flycheck
+    :ensure t))
 
 ;; Projectile : project handling
 (use-package projectile
   :ensure t
   :config
+  (use-package helm-projectile
+    :ensure t)
   (projectile-global-mode))
 
 ;; SmartParens : alternative to paredit
 (use-package smartparens
     :config
+    (use-package evil-smartparens
+      :ensure t)
     (require 'smartparens-config)
     (smartparens-global-mode t)
     (load (expand-file-name "~/.emacs.d/SP.el")))
@@ -160,6 +171,34 @@
 				 (clj-refactor-mode 1)
 				 ;; insert keybinding setup here
 				 )))
+
+(use-package ack
+  :ensure t
+  :config
+  (use-package helm-ack
+    :ensure t))
+
+(use-package rainbow-delimiters
+  :ensure t) 
+
+;; file type mode
+(use-package jedi
+  :ensure t)
+(use-package haskell-mode
+  :ensure t)
+(use-package rust-mode
+  :ensure t)
+(use-package yaml-mode
+  :ensure t)
+(use-package markdown-mode
+  :ensure t)
+(use-package fish-mode
+  :ensure t)
+
+(use-package ace-jump-mode
+  :ensure t) 
+
+;; dash undo-tree org paradox clojure-snippets zenburn-theme
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
